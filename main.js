5b364ea2d1c473ec54d3feb14da548f2f4cd9ae0
Umar <mohammadumar28@gmail.com>
2020-11-06 14:23:36 +0530

    display books list on the page

---
+++ main.js
@@ -0,0 +1,42 @@
+const body = document.querySelector('body');
+
+const container = document.createElement('div');
+
+body.appendChild(container);
+
+const myLibrary = [
+  {
+    title: 'The Hobiit',
+    author: 'Rockefellar',
+    pages: 200,
+    read: 'not yet',
+  },
+];
+
+// function Book() {
+//   // the constructor...
+// }
+
+// function addBookToLibrary() {
+//   // do stuff here
+// }
+
+function displayBooks(arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    const bookCard = document.createElement('div');
+    const title = document.createElement('h3');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const read = document.createElement('p');
+
+    title.textContent = arr[i].title;
+    author.textContent = arr[i].author;
+    pages.textContent = arr[i].pages;
+    read.textContent = arr[i].read;
+
+    bookCard.append(title, author, pages, read);
+    container.appendChild(bookCard);
+  }
+}
+
+displayBooks(myLibrary);
