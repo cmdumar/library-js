c617eeece1fbd133ce23d7fc07e6e607dedb5c80
mohammadumar28 <mohammadumar28@gmail.com>
2020-11-08 20:58:22 +0530

    Implement remove book feature

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const body = document.querySelector('body');
 
 const container = document.createElement('div');
 
+const header = document.createElement('h2');
+header.textContent = 'Books Library';
+
+container.appendChild(header);
+
 body.appendChild(container);
 
 const form = document.createElement('form');
@@ -23,24 +28,41 @@ pages.setAttribute('type', 'number');
 pages.setAttribute('name', 'pages');
 pages.setAttribute('placeholder', 'Number of pages');
 
+const yes = document.createElement('input');
+yes.setAttribute('type', 'radio');
+yes.setAttribute('name', 'read');
+yes.setAttribute('value', 'yes');
+const yesLabel = document.createElement('label');
+yesLabel.setAttribute('for', 'read');
+yesLabel.textContent = 'Yes';
+
+const no = document.createElement('input');
+no.setAttribute('type', 'radio');
+no.setAttribute('name', 'read');
+no.setAttribute('value', 'no');
+const noLabel = document.createElement('label');
+noLabel.setAttribute('for', 'read');
+noLabel.textContent = 'No';
+
 const submit = document.createElement('button');
-submit.setAttribute('type', 'button');
+submit.id = 'submit-btn';
 submit.textContent = 'Add Book';
+
+const readBook = document.createElement('h3');
+readBook.textContent = 'Book Read:';
 
 form.appendChild(title);
 form.appendChild(author);
 form.appendChild(pages);
+form.appendChild(readBook);
+form.appendChild(yesLabel)
+form.appendChild(yes);
+form.appendChild(noLabel);
+form.appendChild(no);
 form.appendChild(submit);
 form.classList.add('display');
 
-const myLibrary = [
-  {
-    title: 'The Hobiit',
-    author: 'Rockefellar',
-    pages: 200,
-    read: 'not yet',
-  },
-];
+const myLibrary = [];
 
 function validateForm(form) {
   if (form['title'].value.length < 5 || form['author'].value.length < 4 || form['pages'].value.length < 1) {
@@ -50,32 +72,81 @@ function validateForm(form) {
   }
 }
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = 'not yet';
+  this.read = read;
+}
+
+Book.prototype.readToggle = function () {
+  if (this.read == 'yes') {
+    this.read = 'no';
+  } else {
+    this.read = 'yes'
+  }
 }
 
 function addBookToLibrary(form) {
-  let book = new Book(form.elements['title'].value, form.elements['author'].value, form.elements['pages'].value);
+  const radio = document.querySelectorAll('input[name="read"]')
+  let selected;
+  for (let r of radio) {
+    if (r.checked) {
+      selected = r.value;
+      break;
+    }
+  }
+
+  let book = new Book(
+      form.elements['title'].value,
+      form.elements['author'].value,
+      form.elements['pages'].value,
+      selected,
+  );
+
+  console.log(book);
+
+  // myLibrary.push({title: book.title, author: book.author, pages: book.pages, read: book.read});
+  myLibrary.push(book);
   displayBook(book);
 }
 
-function displayBook(book) {
-  const bookCard = document.createElement('div');
-  const title = document.createElement('h3');
-  const author = document.createElement('p');
-  const pages = document.createElement('p');
-  const read = document.createElement('p');
+function displayBook() {
+  const books = document.querySelectorAll('.card');
+  books.forEach(function (book) {
+   book.remove();
+  });
 
-  title.textContent = book.title;
-  author.textContent = book.author;
-  pages.textContent = book.pages;
-  read.textContent = book.read;
+  myLibrary.forEach(function (book) {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('card');
+    bookCard.id = (myLibrary.indexOf(book)).toString();
+    const title = document.createElement('h3');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const read = document.createElement('p');
+    const readStatus = document.createElement('button');
+    const deleteBtn = document.createElement('button');
 
-  bookCard.append(title, author, pages, read);
-  container.insertBefore(bookCard, newBtn);
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    read.textContent = book.read;
+    readStatus.textContent = 'Change read status';
+    deleteBtn.textContent = 'Delete book';
+
+    bookCard.append(title, author, pages, read, readStatus, deleteBtn);
+    container.insertBefore(bookCard, newBtn);
+
+    readStatus.onclick = function () {
+      book.readToggle();
+    }
+
+    deleteBtn.addEventListener('click', function() {
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+      document.getElementById(`${bookCard.id}`).remove();
+    });
+  });
 }
 
 const newBtn = document.createElement('button');
@@ -83,9 +154,9 @@ newBtn.textContent = 'New Book';
 
 container.appendChild(newBtn);
 container.appendChild(form);
+displayBook();
 
-
-newBtn.addEventListener('click', function(e) {
+newBtn.addEventListener('click', function() {
   form.classList.toggle('display');
 });
 
@@ -95,6 +166,7 @@ errorMsg.classList.add('display');
 container.insertBefore(errorMsg, form);
 
 submit.addEventListener('click', function(e) {
+  e.preventDefault();
   if (validateForm(form.elements)) {
     addBookToLibrary(form);
     form.classList.toggle('display');
